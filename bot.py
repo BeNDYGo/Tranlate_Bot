fb4224adbec26345c659d722578926b3587e07cb
BeNDYGo <88091698+BeNDYGo@users.noreply.github.com>
2025-08-11 16:49:41 +0300

    Update

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import time
-from translate import get_translate
+from translate import get_translate_wooo, get_translate_google, get_translate
 
 def keyboard(original_word):
     builder = InlineKeyboardBuilder()
@@ -17,26 +17,26 @@ TOKEN = ''
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+sample = f'loading...'
+
 @dp.message(~F.command & F.text)
 async def take_word(message):
     word = message.text
-    sample = f'loading...'
-    bot_message = await message.answer(sample)
-    wooo_translate, google_translate = await get_translate(word)
-    result = google_translate
-    if wooo_translate: await bot_message.edit_text((result), reply_markup=keyboard(word))
-    else: await bot_message.answer((result))
-
+    if '/' in word: return
     print(f'{time.strftime("%H:%M:%S")}|[{message.from_user.first_name} {message.from_user.username}]: {message.text}')
-    print(f'{time.strftime("%H:%M:%S")}|[бот]: {result}')
+    bot_message = await message.answer(sample)
+    google_translate = await get_translate_google(word)
+    bot_message = await bot_message.edit_text(google_translate)
+    print(f'{time.strftime("%H:%M:%S")}|[бот]: {google_translate}')
+    wooo_translate = get_translate_wooo(word)
+    if wooo_translate: await bot_message.edit_text(google_translate, reply_markup=keyboard(word))
 
 @dp.callback_query(F.data.startswith("wooordhunt:"))
 async def callback_handler(callback):
     word = callback.data.split(":", 1)[1]
-    wooo_translate, google_translate = await get_translate(word)
-    result = wooo_translate
-    await callback.message.edit_text(result)
-    print(f'{time.strftime("%H:%M:%S")}|[бот call]: {result}')
+    wooo_translate = get_translate_wooo(word)
+    await callback.message.edit_text(wooo_translate)
+    print(f'{time.strftime("%H:%M:%S")}|[бот call]: {wooo_translate}')
 
 print("Начало нового сеанса")
 async def main(): await dp.start_polling(bot)
