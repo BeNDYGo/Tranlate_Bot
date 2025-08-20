a35a4299a956af6526642b72b51c5646239c9f90
BeNDYGo <palexascha@gmail.com>
2025-08-20 23:14:48 +0300

    update

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import time
 from translate import get_translate_wooo, get_translate_google, get_translate
+from aiogram.filters import Command
+import random
+from aiogram.fsm.state import State, StatesGroup
+import db
 
 def keyboard(original_word):
     builder = InlineKeyboardBuilder()
@@ -12,35 +16,121 @@ def keyboard(original_word):
     )
     return builder.as_markup()
 
-TOKEN = ''
-
-bot = Bot(token=TOKEN)
+TOKEN = '7772137371:AAHr8CudNzCombH0CWf4D5DHacw1Eo3U43E'
+BOT = Bot(token=TOKEN)
 dp = Dispatcher()
-
 sample = f'loading...'
 
-@dp.message(~F.command & F.text)
+
+class AddWord(StatesGroup):
+    waiting_for_word = State()
+    waiting_for_dell = State()
+
+def keyboard(word, translate):
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text='translate',
+        callback_data=f'{word},{translate}'
+    )
+    return builder.as_markup()
+
+def keyboard_translate(word):
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text='wooordhunt',
+        callback_data=f'wooordhunt:{word}'
+    )
+    return builder.as_markup()
+
+def next_word():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text='next',
+        callback_data='next'
+    )
+    return builder.as_markup()
+
+@dp.message(Command("start"))
+async def start(message):
+    user = message.from_user.username
+    words = db.get_words(user)
+    if not words:
+        await message.answer("Добро пожаловать")
+        await message.answer("Ваш список слов пуст")
+    else:
+        word, translate = random.choice(words)[0].split(' - ')
+        await message.answer(word, reply_markup=keyboard(word, translate))
+
+@dp.message(Command("add"))
+async def add(message, state):
+    await message.answer("Введите слово в формате: слово - перевод")
+    await state.set_state(AddWord.waiting_for_word)
+
+@dp.message(AddWord.waiting_for_word)
+async def process_word(message, state):
+    user = message.from_user.username
+    input_text = message.text
+    if ' - ' not in input_text:
+        await message.answer("Неверный формат. Используйте: слово - перевод")
+        return
+    db.add_word(user, input_text)
+    await message.answer("Слово добавлено")
+    await state.clear()
+
+@dp.message(Command("del"))
+async def dell(message, state):
+    await message.answer("Введите слово")
+    await state.set_state(AddWord.waiting_for_dell)
+
+@dp.message(AddWord.waiting_for_dell)
+async def process_dell(message, state):
+    user = message.from_user.username
+    input_text = message.text
+    db.del_word(user, input_text)
+    await message.answer("ок - 👍")
+    await state.clear()
+
+@dp.message(F.text)
 async def take_word(message):
-    word = message.text
-    if '/' in word: return
-    print(f'{time.strftime("%H:%M:%S")}|[{message.from_user.first_name} {message.from_user.username}]: {message.text}')
-    bot_message = await message.answer(sample)
-    google_translate = await get_translate_google(word)
-    bot_message = await bot_message.edit_text(google_translate)
-    print(f'{time.strftime("%H:%M:%S")}|[бот]: {google_translate}')
-    wooo_translate = await get_translate_wooo(word)
-    if wooo_translate: await bot_message.edit_text(google_translate, reply_markup=keyboard(word))
+    if '/' not in message.text:
+        word = message.text
+        print(f'{time.strftime("%H:%M:%S")}|[{message.from_user.first_name} {message.from_user.username}]: {message.text}')
+        bot_message = await message.answer(sample)
+        google_translate = await get_translate_google(word)
+        bot_message = await bot_message.edit_text(google_translate)
+        print(f'{time.strftime("%H:%M:%S")}|[бот]: {google_translate}')
+        wooo_translate = await get_translate_wooo(word)
+        if wooo_translate: await bot_message.edit_text(google_translate, reply_markup=keyboard_translate(word))
 
-@dp.callback_query(F.data.startswith("wooordhunt:"))
-async def callback_handler(callback):
-    word = callback.data.split(":", 1)[1]
-    await callback.message.edit_text(sample)
-    wooo_translate = await get_translate_wooo(word)
-    await callback.message.edit_text(wooo_translate)
-    print(f'{time.strftime("%H:%M:%S")}|[бот call]: {wooo_translate}')
+def next(message, words):
+    word, translate = random.choice(words)[0].split(' - ')
+    return message.edit_text(word, reply_markup=keyboard(word, translate))
 
-print("Начало нового сеанса")
-async def main(): await dp.start_polling(bot)
-asyncio.run(main())
+@dp.callback_query()
+async def callback(query):
+    message = query.message
+    callback_data = query.data
+    user = query.from_user.username
+    words = db.get_words(user)
+    if callback_data == 'next':
+        if not words:
+            await message.answer("Ваш список слов пуст")
+        else:
+            await next(message, words)
+    elif 'wooordhunt:' in callback_data:
+        word = callback_data.split(':')[1]
+        await message.edit_text(sample)
+        wooo_translate = await get_translate_wooo(word)
+        await message.edit_text(wooo_translate)
+        print(f'{time.strftime("%H:%M:%S")}|[бот call]: {wooo_translate}')
+    else:
+        await message.edit_text(f'{callback_data.split(',')[0]} - {callback_data.split(',')[1]}', reply_markup=next_word())
 
 
+async def main():
+    db.init()
+    print("Bot started")
+    await dp.start_polling(BOT)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ db.py
@@ -0,0 +1,86 @@
+import sqlite3
+
+DB_NAME = 'db.db'
+
+db = sqlite3.connect(DB_NAME)
+cursor = db.cursor()
+
+def init():
+    cursor.execute("CREATE TABLE IF NOT EXISTS UsersData (username TEXT, data TEXT)")
+    db.commit()
+
+def add_word(username, inputUser):
+    cursor.execute("INSERT INTO UsersData (username, data) VALUES (?, ?)", (username, inputUser))
+    db.commit()
+
+def get_words(username):
+    cursor.execute("SELECT data FROM UsersData WHERE username = ?", (username,))
+    return cursor.fetchall()
+
+def del_word(username, inputUser):
+    cursor.execute("DELETE FROM UsersData WHERE username = ? AND data = ?", (username, inputUser))
+    db.commit()
+
+def all():
+    cursor.execute("SELECT * FROM UsersData")
+    return cursor.fetchall()
+
+data = ['enough - достаточно',
+'relationship - отношения',
+'friendship - дружба',
+'should - должен',
+'rug - ковёр',
+'awesome - устрашающий',
+'authentic - стентичный/подлинный',
+'the same - такой же',
+'ever - всегда',
+'besides - кроме',
+'leading - ведущий',
+'numb - онемевший',
+'spirit - дух',
+'until - до',
+'lead - вести',
+'clean - чистый',
+'insult - оскорблять',
+'unbearable - невыносимо',
+'around - вокруг',
+'soul - душа',
+'sober - трезвый',
+'violent - жестокий',
+'through - через',
+'pretty - симпатичный',
+'weak - слабый',
+'wild - дикий',
+'waves - волны',
+'madly - безумно',
+'hopelessly - безнадежно',
+'twist - крутить',
+'leading - ведущий',
+'burns - ожоги',
+'fear - страх',
+'throw - бросать',
+'dumpster - мусорный контейнер',
+'interact - взаимодействовать',
+'witch - ведьма',
+'even - даже',
+'means - означать',
+'same - такой же',
+'letting - сдача в аренду',
+'delusions - заблуждение',
+'consume - потреблять',
+'loyalty - верность',
+'fit - подходить',
+'avoid - избегать',
+'god/goddes - бог/богиня',
+'prisoner - заключенный',
+'own - собственный',
+'void - пустота',
+'capture - захватывать/снимать',
+'determine - определять']
+
+if __name__ == "__main__":
+    for i in all():
+        print(i)
+        #add_word('BeNDYGo0', i)
+    db.commit()
+    db.close()
