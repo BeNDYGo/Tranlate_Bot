263175cbb10aaa931f8670f965ca3702a0af3067
BeNDYGo <palexascha@gmail.com>
2025-08-25 16:29:42 +0300

    bug fix

--- bot.py
+++ bot.py
@@ -112,10 +112,9 @@ async def take_word(message):
     if '/' not in message.text:
         word = message.text
         print(f'{time.strftime("%H:%M:%S")}|[{message.from_user.first_name} {message.from_user.username}]: {message.text}')
-        bot_message = await message.answer(sample)
         google_translate = await get_translate_google(word)
         if " " not in word:
-            bot_message = await bot_message.edit_text(google_translate, reply_markup=keyboard_add(word, google_translate))
+            bot_message = await message.answer(google_translate, reply_markup=keyboard_add(word, google_translate))
             print(f'{time.strftime("%H:%M:%S")}|[бот]: {google_translate}')
             wooo_translate = await get_translate_wooo(word)
             if wooo_translate: await bot_message.edit_text(google_translate, reply_markup=keyboard_translate(word, google_translate))
@@ -154,6 +153,7 @@ async def callback(query):
 async def main():
     db.init()
     print("Bot started")
+    await BOT.delete_webhook()
     await dp.start_polling(BOT)
 
 if __name__ == "__main__":
